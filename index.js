a97b612e816380c1e0f58d86b384db77cb313e99
BasharAbdoulah <des.basharabdulah@gmail.com>
2022-05-30 21:38:07 +0300

    deploing

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import postRoutes from "./routes/routes.js";
 import dotenv from "dotenv";
-import join from "path";
 const app = express();
+import path from "path";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 const PORT = process.env.PORT || 8080;
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 dotenv.config();
 app.use(bodyParser.json());
@@ -14,18 +18,19 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 app.use(postRoutes);
 
+//
+app.use(express.static(path.join(__dirname, "/cilent/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/client/build", "index.html"));
+});
+
 // concted to db
 mongoose
-  .connect(`${process.env.DATABASE_URL}`, {
+  .connect(`${process.env.MONGO_URL}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => console.log(" DB is conccted"))
   .catch((err) => console.log(err));
-
-app.use(express.static(join(__dirname, "/client", "build")));
-app.get("*", (_req, res) => {
-  res.sendFile(join(__dirname, "/client", "build", "index.html"));
-});
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
